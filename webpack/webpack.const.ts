f3fcd69bd35593050bf84dc17eb9874b1bbc4ad0
Kernichnyy Andrey <only.exlight@gmail.com>
2020-01-01 01:27:55 +0700

    Update build

--- webpack/webpack.const.ts
+++ webpack/webpack.const.ts
@@ -1,3 +1,3 @@
 export const PRODUCTION = 'production';
 export const DEVELOPMENT = 'development';
-export const BUILD_FOOLDER = '/public';
+export const BUILD_FOOLDER = '/build';
--- webpack/webpack.plugins.ts
+++ webpack/webpack.plugins.ts
@@ -6,31 +6,32 @@ import Dotenv from 'dotenv-webpack';
 
 export const getPlugins = (mode: boolean) => {
     const plugins = [
-        new HtmlWebpackPlugin({
-            title: 'React App',
-            template: './src/index.html',
-            hash: true,
-        }),
-        new BaseHrefWebpackPlugin({
-            baseHref: '/'
-        }),
-        new CopyWebpackPlugin([{
-            from: './src/assets',
-            to: './build/[name].[ext]'
+      new HtmlWebpackPlugin({
+        title: 'Happy new year!',
+        template: './src/index.html',
+        hash: true,
+      }),
+      new BaseHrefWebpackPlugin({
+        baseHref: '/',
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: './src/assets',
+          to: './assets/[name].[ext]',
         },
         {
-            from: './src/favicon.ico',
-            to: './favicon.ico'
-        }
-        ]),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash].css',
-            chunkFilename: '[id].css',
-        }),
-        new Dotenv({
-            path: mode ? './src/enviroment/prod.env' : './src/enviroment/dev.env',
-            silent: false,
-        }),
+          from: './src/favicon.ico',
+          to: './favicon.ico',
+        },
+      ]),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].css',
+      }),
+      new Dotenv({
+        path: mode ? './src/enviroment/prod.env' : './src/enviroment/dev.env',
+        silent: false,
+      }),
     ];
     return plugins;
 };
